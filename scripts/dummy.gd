5230aed0a8ac785710a914089ad027f1e8933c76
JadeAtyla <jamadigal@gmail.com>
2025-05-07 02:20:32 +0800

    cleaning code of dummy

--- scripts/dummy.gd
+++ scripts/dummy.gd
@@ -5,15 +5,8 @@ const JUMP_VELOCITY = -250.0
 
 @onready var animate = $AnimatedSprite2D
 
-#func _physics_process(delta: float) -> void:
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	#move_and_slide()
-
 func _on_body_entered(body: Node2D) -> void:
 	animate.play("hurt")
-
 
 func _on_body_exited(body: Node2D) -> void:
 	animate.play("idle")
